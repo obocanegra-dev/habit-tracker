5241bbe810843b363b0742e18637308bfcb50fb9
Oscar <oscarbocanegraush@gmail.com>
2024-07-12 14:48:21 -0300

    Integrate user routes into the Express server

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/user');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
