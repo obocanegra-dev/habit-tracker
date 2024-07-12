b39d6b0aa91f6446a2627b12d7be72139b995589
Oscar <oscarbocanegraush@gmail.com>
2024-07-12 14:43:30 -0300

    Create User model for MongoDB

---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    habits: [{ type: String }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
